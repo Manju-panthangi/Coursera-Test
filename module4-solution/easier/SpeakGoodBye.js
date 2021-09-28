ac6686abc41e0bbd5c94773c1246586ec3fb7556
Manju-panthangi <manjupanthangi33597@gmail.com>
2021-09-28 15:41:02 +0530

    Solution

---
+++ module4-solution/easier/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+
+(function (window){
+
+  byeSpeaker = {};
+  byeSpeaker.speak = function (name){
+    console.log("Good Bye "+name);
+  };
+  
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ module4-solution/easier/SpeakHello.js
@@ -0,0 +1,10 @@
+
+(function (window){
+  helloSpeaker = {};
+  helloSpeaker.speak = function (name){
+    console.log("Hello "+name);
+  };
+
+  window.helloSpeaker=helloSpeaker;
+
+})(window);
