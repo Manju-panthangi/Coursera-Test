823c84625776783e2c9244ed137208cd6868f93b
Manju-panthangi <manjupanthangi33597@gmail.com>
2022-03-25 18:26:08 +0530

    playing with document object

---
+++ PractiseMod5/main.js
@@ -0,0 +1,10 @@
+function sayHelloToMe(){
+    username = document.getElementById("name").value;
+    
+    newTittle = document.getElementById("tittle").textContent;
+    ; 
+    console.log(newTittle);
+
+    document.getElementById("tittle").newTittle+" "+username;
+
+}
